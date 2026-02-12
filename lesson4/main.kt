61c651d2b335853b7b0bde1544cb3fb05b8f453c
Axel <axelencore@mail.ru>
2026-02-12 08:44:52 +0300

    Initial commit

---
+++ lesson4/main.kt
@@ -0,0 +1,26 @@
+package lesson4
+
+import de.fabmax.kool.KoolApplication   // Запускает Kool-приложение
+import de.fabmax.kool.addScene          // функция - добавить сцену (UI, игровой мир и тд)
+
+import de.fabmax.kool.math.Vec3f        // 3D - вектор (x,y,z)
+import de.fabmax.kool.math.deg          // deg - превращение числа в градусы
+import de.fabmax.kool.scene.*           // Сцена, камера, источники света и тд
+
+import de.fabmax.kool.modules.ksl.KslPbrShader  // готовый PBR Shader - материал
+import de.fabmax.kool.util.Color        // Цветовая палитра
+import de.fabmax.kool.util.Time         // Время deltaT - сколько прошло секунд между двумя кадрами
+
+import de.fabmax.kool.pipeline.ClearColorLoad // Режим говорящий не очищать экран от элементов (нужен для UI)
+
+import de.fabmax.kool.modules.ui2.*     // импорт всех компонентов интерфейса, вроде text, button, Row....
+
+import lesson3.ItemType
+import lesson3.Item
+import lesson3.ItemStack
+import lesson3.HEALING_POTION
+import lesson3.SWORD
+import lesson3.GameState
+import lesson3.EventBus
+import lesson3.putIntoSlot
+import lesson3.useSelected
